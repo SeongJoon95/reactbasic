416b8d1a389ccf0c81d584fa7201bc6056e7e588
SeongJoon95 <gnsxmtkfkd@naver.com>
2024-08-19 12:42:55 +0900

    2024-08-19 12:19

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Component, {ClassComponent,FunctionComponent as F,} from "./component/Component";
+import "./App.css";
+import CurlyBraces from "./component/CurlyBraces";
+
+function App() {
+  return (
+    <>
+      {/* <Component /> */}
+      {/*<ClassComponent />*/}
+      {/*<F />*/}
+      <CurlyBraces />
+    </>
+  );
+}
+
+export default App;
